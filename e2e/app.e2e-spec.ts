6c3d90572f16b410e0221275b666659cf024d4dd
hadlow <me@billyhadlow.com>
2017-10-16 11:38:18 +0100

    All properties are now public

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { CryptochartsPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('cryptocharts App', () => {
-  let page: CryptochartsPage;
+describe('coinrank App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new CryptochartsPage();
+    page = new AppPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
 import { browser, by, element } from 'protractor';
 
-export class CryptochartsPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
