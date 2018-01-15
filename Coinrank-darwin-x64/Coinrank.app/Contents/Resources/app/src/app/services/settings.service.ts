4ea64fe195e70430811b991771695958683fe591
Billy Hadlow <billy@hadlowstudios.com>
2018-01-15 20:40:47 +0000

    Conversion to desktop app

---
+++ Coinrank-darwin-x64/Coinrank.app/Contents/Resources/app/src/app/services/settings.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class SettingsService
+{
+    private settings: string[] = [];
+
+	constructor()
+	{
+		if(localStorage.getItem('settings') != null)
+			this.settings = JSON.parse(localStorage.getItem('settings'));
+		else
+			this.settings = ['24h', 'USD', 'light', 'on'];
+	}
+
+	setSettings(settings)
+	{
+		this.settings = settings;
+		localStorage.setItem('settings', JSON.stringify(settings));
+	}
+
+	getSettings()
+	{
+		return this.settings;
+	}
+}
